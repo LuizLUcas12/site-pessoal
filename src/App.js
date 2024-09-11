9a5a5e1281320024b0320c99a07e97ff7a7329c2
Luiz Lucas <122639068+LuizLUcas12@users.noreply.github.com>
2024-09-11 20:17:37 -0300

    site

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
-// src/App.js
 import React from 'react';
+import Hero from './assets/Hero.png';
+import { Card, Col, Row } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import bike from './assets/BIke.jpeg';
+import academia from './assets/Academia.jpeg';
+import nadar from './assets/Nadar.jpeg';
+
+
 
 
 const App = () => {
@@ -36,19 +43,6 @@ const Main = () => (
   <main>
     <HeroSection />
     <PersonalParagraph />
-    <section>
-      <div className="center">
-        <div className="imagem">
-        </div>
-        <div className="sobre">
-          <h2>Sobre mim</h2>
-          <p>
-            EU sou aluno de Infomatica do IFCM, moro atualmente em Ceará-Mirim no Rio Grande do Norte Brazil. Sou uma pessoa bastante legal só um pouco calado e timido as vezes mas quando pego uma intimidade com alguem chega a ser chato. kkkkk e através dessa pagina web eu irei falar um pouco sobre os meus afazeres do meu Dia a Dia que são esses baixo.<b> BIKE, FUTEBOL, ESTUDO E OUTROS </b> claro que tem muitas coisas mas não vem ao caso,
-            nesse momento irei falar coisas que eu gosto de fazer nos horarios livres e o que eu pretendo com essa linguagem <b>WEB.</b> eu pretendo me aperfeisoar, aprender tudo sobre Html e CSS e poder desenvolver um site interativo e uma pagina criativa. E meu sonho é ter uma vida consideravelmente boa...
-          </p>
-        </div>
-      </div>
-    </section>
     <SectionCuriosidades />
     <SectionCausos />
     <SectionHabilidades />
@@ -58,8 +52,7 @@ const Main = () => (
 const HeroSection = () => (
   <section className="hero">
     <div className="hero-content">
-      <h1>Luiz Lucas Costa Dantas</h1>
-      <img src="https://www.hostinger.com.br/tutoriais/wp-content/uploads/sites/12/2024/02/image-64.webp" alt="Imagem de destaque" className="hero-image" />
+      <img src={Hero} alt="Imagem de destaque" className="hero-image" />
     </div>
   </section>
 );
@@ -67,45 +60,77 @@ const HeroSection = () => (
 const PersonalParagraph = () => (
   <section className="personal-paragraph">
     <div className="center">
-      <h2>
-        Biografia
-      </h2>
+      <h2>Biografia</h2>
       <p>
-        Olá, meu nome é Luiz Lucas Costa Dantas. Sou apaixonado por tecnologia e estou sempre em busca de aprender novas habilidades. Além dos meus estudos em informática, gosto de me envolver em atividades ao ar livre como andar de bicicleta e jogar futebol. Nas horas vagas, também curto jogar videogame e explorar novos hobbies. Acredito que a combinação de aprendizado contínuo e atividades prazerosas é essencial para um equilíbrio saudável na vida. Espero que você goste de conhecer um pouco mais sobre mim através desta página!
+        Olá, meu nome é Luiz Lucas Costa Dantas. Gosto de tecnologia e estou sempre em busca de aprender novas habilidades. Além dos meus estudos em informática, gosto de frequenta academia andar de bicicleta e jogar futebol. Nas horas vagas, também curto jogar jogos online e aprender coisas novas. O aprendizado contínua e atividades prazerosas é essencial para um equilíbrio saudável na vida. Espero que você goste de conhecer um pouco mais sobre mim através desta página!
       </p>
     </div>
   </section>
 );
 
 const SectionCuriosidades = () => (
-  <section>
-    <h2 id="titulo01">Curiosidade</h2>
-    <div className="image-row">
-      <div className="image3">
-        <img className="image" src="imagem/bike.jpg" alt="Bike" />
-        <p>Gosto de andar de bike</p>
-      </div>
-      <div className="image3">
-        <img className="image" src="imagem/academia.webp" alt="Academia" />
-        <p>Gosto de academia</p>
-      </div>
-      <div className="image3">
-        <img className="image" src="imagem/call of duty.jfif" alt="Call of Duty" />
-        <p>Gosto de jogar call of duty</p>
-      </div>
-    </div>
-    <div className="image-row">
-      <div className="image3">
-        <img className="image" src="imagem/futebol.jpg" alt="Futebol" />
-        <p>Gosto de Jogar bola</p>
-      </div>
-      <div className="image3">
-        <img className="image" src="imagem/images.jfif" alt="Nadar" />
-        <p>Gosto de Nadar</p>
-      </div>
-    </div>
+  <section className="section">
+    <h2 id="titulo01" className="centered-title">Curiosidades</h2>
+    <Row xs={1} md={2} lg={3} className="g-4">
+      <Col>
+        <Card>
+          <Card.Img variant="top" src={bike} alt="Bike" />
+          <Card.Body>
+            <Card.Title>Andar de Bike</Card.Title>
+            <Card.Text>
+              Gosto de andar de bike.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </Col>
+      <Col>
+        <Card>
+          <Card.Img variant="top" src={academia} alt="Academia" />
+          <Card.Body>
+            <Card.Title>Academia</Card.Title>
+            <Card.Text>
+              Gosto de academia.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </Col>
+      <Col>
+        <Card>
+          <Card.Img variant="top" src="imagem/call of duty.jfif" alt="Call of Duty" />
+          <Card.Body>
+            <Card.Title>Call of Duty</Card.Title>
+            <Card.Text>
+              Gosto de jogar Call of Duty.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </Col>
+      <Col>
+        <Card>
+          <Card.Img variant="top" src="imagem/futebol.jpg" alt="Futebol" />
+          <Card.Body>
+            <Card.Title>Futebol</Card.Title>
+            <Card.Text>
+              Gosto de jogar bola.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </Col>
+      <Col>
+        <Card>
+          <Card.Img variant="top" src={nadar} alt="Nadar" />
+          <Card.Body>
+            <Card.Title>Nadar</Card.Title>
+            <Card.Text>
+              Gosto de nadar.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </Col>
+    </Row>
   </section>
 );
+
 
 const SectionCausos = () => (
   <section>
@@ -119,9 +144,9 @@ const SectionCausos = () => (
 
 const SectionHabilidades = () => (
   <section>
-    <h2 id="titulo04">Habilidade</h2>
+    <h2 id="titulo04">Habilidades</h2>
     <p>Quando eu era mais pequeno eu tinha muita habilidade em jogar bola, mas com o passar dos anos foi perdendo esse gás que tinha por falta de não treinar mais, é então só jogo as vezes com os amigos.</p>
-    <p>Também tinha uma Habilidade em desenhar mais não foquei nessa aréa</p>
+    <p>Também tinha uma habilidade em desenhar, mas não foquei nessa área.</p>
   </section>
 );
 
